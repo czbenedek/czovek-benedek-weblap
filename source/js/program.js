d1bcf306edea91d011c70ccfabd53d0713229fa8
czbenedek <benedek.czovek@stud.uni-corvinus.hu>
2024-11-26 11:21:02 +0100

    Add files via upload

--- source/js/program.js
+++ source/js/program.js
@@ -1,8 +1,19 @@
-button.addEventListener('mouseenter', () => {
-    button.style.backgroundImage = `url(${hoverImage})`;
-  });
 
-  // Remove the background on mouse leave
-  button.addEventListener('mouseleave', () => {
-    button.style.backgroundImage = '';
-  });
+
+const gomb1 = document.getElementById('aktiv');
+
+
+// ide még egy vérakozást be kell rakni
+gomb1.addEventListener('mouseenter', () => {
+    gomb1.style.backgroundImage = `url('source/icon.png')`;
+});
+
+gomb1.addEventListener('mouseleave', () => {
+    gomb1.style.backgroundImage = ``;
+});
+
+
+// Remove the background on mouse leave
+//   button.addEventListener('mouseleave', () => {
+//     button.style.backgroundImage = '';
+//   });
