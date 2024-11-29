9d8281c65a0130463905f661bd0ed80876c13212
czbenedek <benedek.czovek@stud.uni-corvinus.hu>
2024-11-29 16:10:28 +0100

    Add files via upload

--- source/js/program.js
+++ source/js/program.js
@@ -1,19 +1,67 @@
+document.getElementById(`ellenorzes`).addEventListener(`click`, jelszoEll)
+
+function jelszoEll() {
+    const jelszo = document.getElementById("jelszobox").value;
+
+   
+    const kis = `abcdefghijklmnopqrstuvwxyz`;
+    const nagy = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    const szam = `0123456789`;
+    const spec = `.,!?:@#$%*-_`;
+    let vankis = false;
+    let vannagy = false;
+    let vanszam = false;
+    let vanspec = false;
+    let baj = false;
+    
+    for (let i = 0; i < jelszo.length; i++) {
+        const betu = jelszo[i];
+
+        
+        if (kis.indexOf(betu) !== -1) {
+            vankis = true;
+        }
+        if (nagy.indexOf(betu) !== -1) {
+            vannagy = true;
+        }
+        if (szam.indexOf(betu) !== -1) {
+            vanszam = true;
+        }
+        if (spec.indexOf(betu) !== -1) {
+            vanspec = true;
+        }
+    }
+
+    let kiiras = "A jelszónak tartalmaznia kell"
+    if (vankis == false) {
+        kiiras = kiiras + " kisbetűt,";
+        baj=true
+    }
+    if (vannagy == false) {
+        kiiras = kiiras + " Nagybetűt,";
+        baj=true
+    }
+    if (vanszam == false) {
+        kiiras = kiiras + " 5z4`m0t,";
+        baj=true
+    }
+    if (vanspec == false) {
+        kiiras = kiiras + " $pec|áL|$ karaktert (pl: .,!?),";
+        baj=true
+    }
+    kiiras = kiiras.substring(0,(kiiras.length)-1)
+   
+    kiiras = kiiras + " is"
 
 
-const gomb1 = document.getElementById('aktiv');
+        if (baj==true) {
 
 
-// ide még egy vérakozást be kell rakni
-gomb1.addEventListener('mouseenter', () => {
-    gomb1.style.backgroundImage = `url('source/icon.png')`;
-});
+            alert(`${kiiras}`)
+        }
 
-gomb1.addEventListener('mouseleave', () => {
-    gomb1.style.backgroundImage = ``;
-});
+        else {
+            alert(`Köszönjük a részvételt`)
+        }
 
-
-// Remove the background on mouse leave
-//   button.addEventListener('mouseleave', () => {
-//     button.style.backgroundImage = '';
-//   });
+}
